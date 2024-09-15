5e03f1df412679e7e8c4fda864ea89d971d041f4
orlilevine <orlirosenthal@gmail.com>
2024-09-15 22:38:11 +0300

    Update purchasedshiurs.blade.php

--- resources/views/user/purchasedshiurs.blade.php
+++ resources/views/user/purchasedshiurs.blade.php
@@ -5,20 +5,19 @@
 @section('content')
     <div class="container">
         <div class="row">
-
-                    @foreach($series->shiurs as $shiur)
-                        <div class="mt-2">
-                            <h5>{{ $shiur->title }}</h5>
-                            <p>{{ $shiur->description }}</p>
-                            <audio controls>
-                                <source src="{{ asset($shiur->recording_path) }}" type="audio/mpeg">
-                                Your browser does not support the audio element.
-                            </audio>
-                            <a href="{{ asset( $shiur->recording_path) }}" download class="btn btn-success mt-2">Download</a>
-
-                        </div>
-                    @endforeach
+            @foreach($series->shiurs as $shiur)
+                <div>
+                    <h5>{{ $shiur->title }}</h5>
+                    <p>{{ $shiur->description }}</p>
+                    <audio controls>
+                        <source src="{{ asset($shiur->recording_path) }}" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>
+                    <br>
+                    <!-- Move the button below the audio element and apply inline styling -->
+                    <a href="{{ asset($shiur->recording_path) }}" download class="btn" style="background-color: slategray; color: white; margin-top: 10px; display: inline-block; padding: 5px 10px; text-align: center; text-decoration: none;">Download</a>
                 </div>
+            @endforeach
         </div>
     </div>
 @endsection
