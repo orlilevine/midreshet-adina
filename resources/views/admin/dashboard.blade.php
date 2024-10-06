5c0f29c45f27be80a743ed5a8fbea371c10692d9
orlilevine <orlirosenthal@gmail.com>
2024-10-06 22:33:57 +0300

    Slate gray to blue

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,25 +13,25 @@
 
         <div class="row">
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.shiur.create') }}" class="btn btn-block" style="background-color: slategray; color: white;">Create Shiur</a>
+                <a href="{{ route('admin.shiur.create') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">Create Shiur</a>
             </div>
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.series.create') }}" class="btn btn-block" style="background-color: slategray; color: white;">Create Series</a>
+                <a href="{{ route('admin.series.create') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">Create Series</a>
             </div>
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.speaker.create') }}" class="btn btn-block" style="background-color: slategray; color: white;">Create Speaker</a>
+                <a href="{{ route('admin.speaker.create') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">Create Speaker</a>
             </div>
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.shiur.edit') }}" class="btn btn-block" style="background-color: slategray; color: white;">Edit Shiur</a>
+                <a href="{{ route('admin.shiur.edit') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">Edit Shiur</a>
             </div>
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.series.edit') }}" class="btn btn-block" style="background-color: slategray; color: white;">Edit Series</a>
+                <a href="{{ route('admin.series.edit') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">Edit Series</a>
             </div>
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.shiurStats') }}" class="btn btn-block" style="background-color: slategray; color: white;">View Shiur Stats</a>
+                <a href="{{ route('admin.shiurStats') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">View Shiur Stats</a>
             </div>
             <div class="col-md-12 mb-3">
-                <a href="{{ route('admin.messages') }}" class="btn btn-block" style="background-color: slategray; color: white;">View Messages</a>
+                <a href="{{ route('admin.messages') }}" class="btn btn-block" style="background-color: #001f3f; color: white;">View Messages</a>
             </div>
         </div>
 
