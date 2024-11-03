f24dfddc199c09f6f600a51b7f5483de5623a3c3
orlilevine <orlirosenthal@gmail.com>
2024-11-03 09:49:41 +0200

    Purchase success link to go to my shiurim

--- resources/views/payments/success.blade.php
+++ resources/views/payments/success.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('title', 'Payment Successful!')
+
 @section('content')
     <div class="container">
         <p>Thank you for your purchase. Your payment has been processed successfully.</p>
+        <p><a href="{{ url('/purchased-series') }}" class="cta-button" style="color: #001f3f;">Go to My Shiurim</a></p>
     </div>
 @endsection
