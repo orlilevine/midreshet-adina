d15801e8983ec378bb2743b558cda5b02b4126b8
orlilevine <orlirosenthal@gmail.com>
2024-11-26 11:33:56 +0200

    Switching about us background pic

--- resources/views/navbar/about.blade.php
+++ resources/views/navbar/about.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-    <div class="container-fluid" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('{{ asset('images/KotelCloseUp.png') }}'); background-size: cover; background-position: center; height: 100vh; display: flex; align-items: center;">
+    <div class="container-fluid" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('{{ asset('images/KotelNew.jpg') }}'); background-size: cover; background-position: center; height: 100vh; display: flex; align-items: center;">
         <div class="container text-center" style="padding: 40px;">
             <h1 class="mb-4" style="color: #001f3f; font-size: 3rem; text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);">About Midreshet Adina</h1>
             <p class="lead" style="color: black;">
